de7de78db60bb7d91631c0db3404cbb66b9d05a1
wesley-paiva-vega <wesley.snybool.dev@gmail.com>
2023-07-20 16:38:11 -0300

    feat(fe-44/fe-43): definindo os validators

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { UserDto } from 'src/dto/users/users.dto';
 import { UserService } from 'src/services/user/user.service';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @Get('list')
-  findAll(): string {
-    return 'get all the fucking list';
+  @Get()
+  findAll(): Promise<UserDto[]> {
+    return this.userService.findAll();
   }
 }
--- src/entities/users/users.module.ts
+++ src/entities/users/users.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from 'src/controllers/user/user.controller';
+import { UserService } from 'src/services/user/user.service';
+import { UserEntity } from './users.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  controllers: [UserController],
+  providers: [UserService],
+})
 export class UsersModule {}
