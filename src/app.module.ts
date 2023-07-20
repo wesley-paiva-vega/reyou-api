7aeac7d79e31dc4aecc18e0fdae954bd75b7ab52
wesley-paiva-vega <wesley.snybool.dev@gmail.com>
2023-07-20 14:00:13 -0300

    feat(fe-44/fe-43): primeiros gets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
+import { UserController } from './controllers/user/user.controller';
 import { UsersModule } from './entities/users/users.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
-  controllers: [],
+  controllers: [UserController],
   providers: [],
 })
 export class AppModule {}
