29a33953e05896bdc99751c46d03bb5089040c54
wesley-paiva-vega <wesley.snybool.dev@gmail.com>
2023-07-21 09:11:35 -0300

    restart

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
-import { UserController } from './controllers/user/user.controller';
-import { UserModule } from './entities/users/users.module';
-import { UserService } from './services/user/user.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule],
-  providers: [UserService],
-  controllers: [UserController],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  providers: [],
+  controllers: [],
 })
 export class AppModule {}
--- src/controllers/user/user.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { UserService } from 'src/services/user/user.service';
-
-@Controller('users')
-export class UserController {
-  constructor(private readonly userService: UserService) {}
-}
